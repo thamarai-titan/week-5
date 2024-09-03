22e2a03fdcd8fd5643d43f8f98050bef68aa9ff3
Thamarai <tmanalan710@gmail.com>
2024-09-03 05:58:41 +0530

    Working with the form

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
-const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+const app = express();
+
+app.use(express.json());
+
+var Users = [];
+
+app.get('/login', (req, res) => {
+  res.render('login');
 })
 
-app.listen(3000)
+app.post('/login', (req, res)=>{
+  res.send(`Full name is:${req.body.user} ${req.body.password}.`);
+})
+app.listen(3000);
